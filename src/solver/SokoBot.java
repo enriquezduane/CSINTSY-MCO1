26a23cc53c5d2517be0bf76f83f7da84b2bcffc5
Duane Enriquez <duanequez@gmail.com>
2023-09-22 18:06:47 +0800

    added classes for nodes and points

--- src/solver/SokoBot.java
+++ src/solver/SokoBot.java
@@ -1,22 +1,34 @@
 package solver;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+class Node {
+  int playerX, playerY;
+  List<Point> cratePositions;
+  int g, h, f;
+  Node parent;
+}
+
+class Point {
+  int x, y;
+
+  Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
 public class SokoBot {
 
   public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
-    /*
-     * YOU NEED TO REWRITE THE IMPLEMENTATION OF THIS METHOD TO MAKE THE BOT SMARTER
-     */
-    /*
-     * Default stupid behavior: Think (sleep) for 3 seconds, and then return a
-     * sequence
-     * that just moves left and right repeatedly.
-     */
+    PriorityQueue<Node> openList = new PriorityQueue<>(Comparator.comparingInt(node -> node.f));
     try {
       Thread.sleep(3000);
     } catch (Exception ex) {
       ex.printStackTrace();
     }
-    return "rdl";
+    return "udlr";
   }
-
 }
