d0c1d168269e4dc0181cc89da0574f053a549338
Duane Enriquez <duanequez@gmail.com>
2023-09-22 18:21:01 +0800

    added initializers

--- src/solver/SokoBot.java
+++ src/solver/SokoBot.java
@@ -1,5 +1,6 @@
 package solver;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -7,8 +8,22 @@ import java.util.PriorityQueue;
 class Node {
   int playerX, playerY;
   List<Point> cratePositions;
-  int g, h, f;
+  int f, g, h;
   Node parent;
+
+  Node(int playerX, int playerY, List<Point> cratePositions) {
+    this.playerX = playerX;
+    this.playerY = playerY;
+    this.cratePositions = cratePositions;
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+    this.parent = null;
+  }
+
+  public void printPosition() {
+    System.out.println("( " + playerX + "," + playerY + " )");
+  }
 }
 
 class Point {
@@ -24,11 +39,27 @@ public class SokoBot {
 
   public String solveSokobanPuzzle(int width, int height, char[][] mapData, char[][] itemsData) {
     PriorityQueue<Node> openList = new PriorityQueue<>(Comparator.comparingInt(node -> node.f));
-    try {
-      Thread.sleep(3000);
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
+    Node startNode = initializeStartNode(itemsData);
+    openList.add(startNode);
+    startNode.printPosition();
+
     return "udlr";
+  }
+
+  public Node initializeStartNode(char[][] itemsData) {
+    int playerX = -1, playerY = -1;
+    List<Point> cratePositions = new ArrayList<>();
+
+    for (int y = 0; y < itemsData.length; y++) {
+      for (int x = 0; x < itemsData[y].length; x++) {
+        if (itemsData[y][x] == '@') {
+          playerX = x;
+          playerY = y;
+        } else if (itemsData[y][x] == '$') {
+          cratePositions.add(new Point(x, y));
+        }
+      }
+    }
+    return new Node(playerX, playerY, cratePositions);
   }
 }
